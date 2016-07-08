d0eed95e5af8eff015df5b2098435e4aa18250ca
k-nishigaki <k-nishigaki@nekojarashi.com>
2016-07-08 14:36:14 +0900

    lunch time

--- school_uniform.py
+++ school_uniform.py
@@ -1,16 +1,20 @@
 card_order = ["0","3","4","5","6","7","8","9","10","J","Q","K","A","2"]
 
 given_cards = raw_input().split()
-field = "0"
-rank = 0
+rank = [0 for i in range(0, len(given_cards))]
 
-while len(given_cards):
-    print(given_cards)
-    tmp_list = given_cards
-    for idx, card in enumerate(tmp_list):
+while given_cards.count("0") < 52:
+    #print(given_cards)
+    for rank, card in given_cards:
         if card_order.index(card) > card_order.index(field):
-            field = given_cards.pop(idx)
-            rank += 1
-            print str(rank) + ": ",
-            print("dbg: " + str(card) + " " + str(idx) + " " + str(field))
-    
+            field = given_cards[idx]
+            given_cards[idx] = "0"
+            #print str(rank) + ": ",
+            print("idx: " + str(idx) + " card: " + str(card) + " field: " + str(field))
+            #print(str(idx + 1))
+    else:
+        field = "0"
+
+def rebuild_list(list=[], split_index):
+    tmp = list[0:split_index]
+
