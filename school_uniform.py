bdcd0f4b36b2ad04260b1b07c3419bbbb2b82313
k-nishigaki <k-nishigaki@nekojarashi.com>
2016-07-09 17:54:53 +0900

    success school_uniform

--- school_uniform.py
+++ school_uniform.py
@@ -1,20 +1,33 @@
 card_order = ["0","3","4","5","6","7","8","9","10","J","Q","K","A","2"]
 
 given_cards = raw_input().split()
-rank = [0 for i in range(0, len(given_cards))]
+rank_list = [0 for i in range(0,len(given_cards))]
+num_card = len(given_cards)
+loop_end = num_card - 1
+fielded_idx = -1
+field = "0"
+loop_idx = 0
+rank = 0
 
-while given_cards.count("0") < 52:
-    #print(given_cards)
-    for rank, card in given_cards:
+while given_cards.count("0") < num_card:
+    while loop_idx != fielded_idx: 
+       
+        card = given_cards[loop_idx]
+
         if card_order.index(card) > card_order.index(field):
-            field = given_cards[idx]
-            given_cards[idx] = "0"
-            #print str(rank) + ": ",
-            print("idx: " + str(idx) + " card: " + str(card) + " field: " + str(field))
-            #print(str(idx + 1))
-    else:
-        field = "0"
+            fielded_idx = loop_idx
+            field = given_cards[loop_idx]
+            given_cards[loop_idx] = "0"
+            rank += 1
+            rank_list[loop_idx] = rank
 
-def rebuild_list(list=[], split_index):
-    tmp = list[0:split_index]
+        loop_idx += 1
 
+        if loop_idx == num_card:
+            loop_idx = 0
+    
+    field = "0"
+    loop_idx = loop_idx + 1 if loop_idx < loop_end else 0
+
+for p in rank_list:
+    print(str(p))
